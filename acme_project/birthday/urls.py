3245145d04e7d78bb4df0b4b8eb42e3979299143
Roman Dobrikov <dobrikovroma@yandex.ru>
2026-03-04 21:53:10 +0300

    Тема урока "Создание записей в БД"

---
+++ acme_project/birthday/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'birthday'
+
+urlpatterns = [
+    path('', views.birthday, name='create'),
+    path('list/', views.birthday_list, name='list')
+]
