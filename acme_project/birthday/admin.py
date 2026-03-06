c40c24977ce88afc5cf2cbfbc3c5b23d5e990331
Roman Dobrikov <dobrikovroma@yandex.ru>
2026-03-06 19:07:47 +0300

    Конец спринт 9 тема 2/3

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,20 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_editable = (
+        'birthday',
+    )
+    search_fields = ('first_name',)
+    list_display_links = ('first_name',)
+
+@admin.register(Birthday)
+class MyAdminPanel(admin.ModelAdmin):
+    pass
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,9 +1,10 @@
 # birthday/forms.py
 from django import forms
+from django.core.exceptions import ValidationError
 
 # Импортируем класс модели Birthday.
 from .models import Birthday
-
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 # Для использования формы с моделями меняем класс на forms.ModelForm.
 class BirthdayForm(forms.ModelForm):
@@ -12,9 +13,30 @@ class BirthdayForm(forms.ModelForm):
     class Meta:
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
+        
         widgets = {
-            'birthday': forms.DateInput(attrs={'type': 'date'})
-            
+            'birthday': forms.DateInput(
+                attrs={'type': 'date'},
+                format='%Y-%m-%d'
+            )
         }
         # Указываем, что надо отобразить все поля.
         fields = '__all__'
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
